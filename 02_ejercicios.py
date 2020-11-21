c5597bd05a039040abc89b39fdfcebd31f41d98a
usuario01 <usuario01@gmail.com>
2020-11-20 21:18:16 -0500

    Update

---
+++ 02_ejercicios.py
@@ -0,0 +1,22 @@
+'''
+Crear la clase Persona
+
+- Crear atributos:
+   * nombre
+   * apelido
+   * dni
+
+- Crear atributo de clase
+   * pais
+   * listados_de_personas
+
+- crear un metodo que me obtenga
+  el [apellido : dni] , por ejemplo
+  "[Gomez : 1234567]"
+
+- Cada vez que se instancia un clase, 
+  se debe agregar el nombre de la persona
+  a un atributo de clase llamado listado_de_personas
+
+'''
+
