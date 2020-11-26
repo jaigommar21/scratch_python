e91ce2f38aadc43ebf731e46fc41097387490dad
usuario01 <usuario01@gmail.com>
2020-11-25 20:00:00 -0500

    Upgrade

---
+++ 04_ejercicios.py
@@ -0,0 +1,38 @@
+'''
+Ejercicio Adicional:
+Crear la clase base Persona , que tenga el atributo
+nombre, apellido y dni ; crear la clase hija Estudiante 
+que inicialice el nombre, apellido y dni. Agrege el método
+obtener_dni en la clase base y usarla desde una instancia
+de la clase hija.
+'''
+
+# Clase Base
+class Persona:
+
+    def __init__(self, nombre, apellido, dni):
+        super().__init__()
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+   
+    def obtener_dni(self):
+        return self.dni
+
+# Clase hija
+class Vecino(Persona):
+    pass
+
+
+per_01 = Persona("David","Perez","45698523")
+print(per_01.obtener_dni())
+vec_01 = Vecino("Joel","Duque","89562355")
+print(vec_01.obtener_dni())
+
+
+'''
+Sobrescribir el metodo obtener_dni en la clase Estudiante
+para que siempre retorne "DDD", usarlo en un instancia 
+de la clase Estudiante.
+
+'''
